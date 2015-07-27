425ec824ec8f428f36ab6af673a3fc3ed09668a5
arex <arexsm@126.com>
2015-07-28 01:19:47 +0800

    学习了查询操作

--- Application/Home/Controller/FormController.class.php
+++ Application/Home/Controller/FormController.class.php
@@ -9,14 +9,31 @@
 namespace Home\Controller;
 
 use Think\Controller;
+use Think\Model;
 
 class FormController extends Controller
 {
     public function index()
     {
         $Form = M('Form');
-        $list = $Form->order('create_time asc')->select();
-        $this->assign('list',$list);
+        $formCount = $Form->count();
+        $this->assign('count', $formCount);
+//        各种查询条件
+        $map['id'] = array('egt', 0);
+//        $map['id'] = array('between',array('1','8'));
+//        $map['title'] = array('like','thinkphp%');
+//        $map['id'] = array(array('neq', 9), array('gt', 3), 'and');
+        $list = $Form->where($map)->order('create_time asc')->select();
+
+//        直接SQL查询
+//        $Model = new Model(); // 实例化一个model对象 没有对应任何数据表
+//        $list = $Model->query("SELECT * FROM think_form ORDER BY create_time asc");
+
+//        只生成SQL语句，不查询
+//        $subQuery = $Form->where($map)->order('create_time asc')->buildSql();
+//        echo($subQuery);
+
+        $this->assign('list', $list);
         $this->display();
     }
 
@@ -26,7 +43,7 @@ class FormController extends Controller
         if ($Form->create()) {
             $result = $Form->add();
             if ($result) {
-                $this->success('数据添加成功！', U('Home/Form/index'));
+                $this->success('数据添加成功！', U('Form/index'));
             } else {
                 $this->error('数据添加错误！');
             }
@@ -61,7 +78,7 @@ class FormController extends Controller
         if ($Form->create()) {
             $result = $Form->save();
             if ($result) {
-                $this->success('操作成功！', U('Home/Form/index'));
+                $this->success('操作成功！', U('Form/index'));
             } else {
                 $this->error('写入错误！');
             }
@@ -78,7 +95,7 @@ class FormController extends Controller
         $Form->content = 'ThinkPHP3.2.3版本发布';
         $result = $Form->where('id=' . $id)->save(); // 根据条件保存修改的数据
         if ($result) {
-            $this->success('操作成功！', U('Home/Form/read/id/' . $id));
+            $this->success('操作成功！', U('Form/read/id/' . $id));
         } else {
             $this->error('写入错误！');
         }
@@ -89,7 +106,7 @@ class FormController extends Controller
         $Form = M("Form");
         $result = $Form->delete($id);
         if ($result) {
-            $this->success('操作成功！', U('Home/Form/index'));
+            $this->success('操作成功！', U('Form/index'));
         } else {
             $this->error('写入错误！');
         }
